263bf9e8e4b2f28ce36b2f6e0de4b448339d1f5a
Bekzod7777 <100689033+Bekzod7777@users.noreply.github.com>
2022-03-09 17:11:34 +0500

    Update number_even.py

--- number_even.py
+++ number_even.py
@@ -3,7 +3,6 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of even digits in the variable "var_int".
-a=4414
+a=4444
 b=((a%10)%2)+((a//10)%10)%2+(((a//100)%10)%2)+(a//1000)%2
-
-print(b,'ta toq son')
+print(4-b,'ta juft son')
